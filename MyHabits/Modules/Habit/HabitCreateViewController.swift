3564960dfbad4940cdf70af43d9259f6498c2f48
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-11-07 13:42:00 +0300

    HabitCreateViewController UI is done yet is not working

--- MyHabits/Modules/Habit/HabitCreateViewController.swift
+++ MyHabits/Modules/Habit/HabitCreateViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class HabitCreateViewController: UIViewController {
+class HabitCreateViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - Subviews
 
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "Title"
+        label.text = "TITLE"
         label.font = .boldSystemFont(ofSize: 16)
         label.textColor = UIColor(named: "textColor")
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -25,25 +25,53 @@ class HabitCreateViewController: UIViewController {
         textField.placeholder = "Morning jogging, 8 hours sleep etc..."
         textField.font = .systemFont(ofSize: 16)
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.delegate = self
         return textField
     }()
     
     private lazy var colorLabel: UILabel = {
         let label = UILabel()
-        label.text = "Color"
+        label.text = "COLOR"
         label.font = .boldSystemFont(ofSize: 16)
         label.textColor = UIColor(named: "textColor")
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private lazy var colorCircle: ColorCircle = {
+        let circle = ColorCircle()
+        circle.translatesAutoresizingMaskIntoConstraints = false
+        return circle
+    }()
+    
     private lazy var timeLabel: UILabel = {
         let label = UILabel()
-        label.text = "Time"
+        label.text = "TIME"
         label.font = .boldSystemFont(ofSize: 16)
         label.textColor = UIColor(named: "textColor")
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var timeSelection: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let firstAttribute = [ NSAttributedString.Key.foregroundColor: UIColor(named: "textColor") ?? .label ]
+        let firstString = NSAttributedString(string: "Daily at ", attributes: firstAttribute)
+
+        let secondAttribute = [ NSAttributedString.Key.foregroundColor: UIColor.link, NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+        let secondString = NSAttributedString(string: "15:00", attributes: secondAttribute)
+
+        let resultString = NSMutableAttributedString()
+        resultString.append(firstString)
+        resultString.append(secondString)
+        
+        button.setAttributedTitle(resultString, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.contentHorizontalAlignment = .left
+        
+        return button
     }()
     
     override func viewDidLoad() {
@@ -61,9 +89,14 @@ class HabitCreateViewController: UIViewController {
     private func addSubviews() {
         view.addSubview(titleLabel)
         view.addSubview(titleTextField)
+        view.addSubview(colorLabel)
+        view.addSubview(colorCircle)
+        view.addSubview(timeLabel)
+        view.addSubview(timeSelection)
     }
     
     private func setupConstraints() {
+
         let safeAreaGuide = view.safeAreaLayoutGuide
 
         NSLayoutConstraint.activate([
@@ -71,7 +104,25 @@ class HabitCreateViewController: UIViewController {
             titleLabel.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 16),
             
             titleTextField.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
-            titleTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16)
+            titleTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            titleTextField.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16),
+            
+            colorLabel.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
+            colorLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 16),
+            colorLabel.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16),
+            
+            colorCircle.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
+            colorCircle.topAnchor.constraint(equalTo: colorLabel.bottomAnchor, constant: 16),
+            colorCircle.heightAnchor.constraint(equalToConstant: 32),
+            colorCircle.widthAnchor.constraint(equalToConstant: 32),
+            
+            timeLabel.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
+            timeLabel.topAnchor.constraint(equalTo: colorCircle.bottomAnchor, constant: 16),
+            timeLabel.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16),
+            
+            timeSelection.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16),
+            timeSelection.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 16),
+            timeSelection.trailingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 128)
         ])
     }
     
@@ -91,5 +142,13 @@ class HabitCreateViewController: UIViewController {
     
     @IBAction func cancelButtonTapped(sender: AnyObject) {
         print("Cancel button tapped")
+    }
+    
+    // Setting character limit for UITextField (50 characters)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        return updatedText.count <= 50
     }
 }
