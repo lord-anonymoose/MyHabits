2e9a4a6064c4e3d4afa8b44d0b9b1a725f6f4089
Philipp Lazarev <philipp.lazareff@gmail.com>
2023-11-10 00:30:51 +0300

    TestViewController cleaned up. Habit can be created through HabitCreateViewController

--- MyHabits/Modules/Habit/TestViewController.swift
+++ MyHabits/Modules/Habit/TestViewController.swift
@@ -9,21 +9,6 @@ import UIKit
 
 class TestViewController: UIViewController {
     
-    private lazy var timeLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "TIME"
-        label.font = .boldSystemFont(ofSize: 16)
-        label.textColor = UIColor(named: "textColor")
-        return label
-    }()
-    
-    private lazy var timePickerView: UITimePickerView = {
-        let timePickerView = UITimePickerView()
-        timePickerView.translatesAutoresizingMaskIntoConstraints = false
-        return timePickerView
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -36,26 +21,17 @@ class TestViewController: UIViewController {
     }
     
     func addSubviews() {
-        view.addSubview(timePickerView)
     }
     
     func setupConstraints() {
         let safeAreaGuide = view.safeAreaLayoutGuide
 
         NSLayoutConstraint.activate([
-            timePickerView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor),
-            timePickerView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor),
-            timePickerView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
-            timePickerView.heightAnchor.constraint(equalToConstant: 112)
         ])
     }
     
     @objc func didTapColorSave(sender: UIDatePicker) {
-        let timeFormatter = DateFormatter()
-        timeFormatter.timeStyle = DateFormatter.Style.short
 
-        //var strDate = timeFormatter.string(from: timePickerView.date)
-        //print(strDate)
     }
 }
 
